3b0d11f8dc14098c85ac971ad567bbb6baf72d86
Ильяс Юсупов <iu70980@gmail.com>
2023-01-12 11:04:59 +0300

    Task2

--- wrcrft/Program.cs
+++ wrcrft/Program.cs
@@ -13,16 +13,14 @@ namespace wrcrft
             targetFootMan.Name = "Влад";
             //Unit Archer = unitMaker.Make("archer");
             //Mage mage = unitMaker.MagicMake();
-            BlackSmith smith = new BlackSmith(unitMaker);
+            /*BlackSmith smith = new BlackSmith(unitMaker);*/
+            
             Console.WriteLine($"Урон {footMan.Name}: {footMan.Damage}");
             Console.WriteLine($"Броня {footMan.Name}: {footMan.Armor}");
             Console.WriteLine($"Урон {targetFootMan.Name}: {targetFootMan.Damage}");
             Console.WriteLine($"Броня {targetFootMan.Name}: {targetFootMan.Armor}");
             Task.Run(() => Fight(targetFootMan, footMan));
             Task.Run(() => Fight(footMan, targetFootMan)).Wait();
-
-
-
 
             //BlackSmith smith = new BlackSmith(unitMaker);
             //smith.UpgradeDamage();
@@ -47,19 +45,20 @@ namespace wrcrft
             //    Console.WriteLine($"Здоровье footMan после лечения: {footMan.RealHealthPoint}");
             //    Console.WriteLine($"Оставшееся количество маны: {mage.Mane} \n");
             //}
-
-            Console.WriteLine("Итог:");
-            Console.WriteLine($"Оставшеeся здоровье {footMan.Name}: {footMan.RealHealthPoint}");
-            Console.WriteLine($"Оставшеeся здоровье {targetFootMan.Name}: {targetFootMan.RealHealthPoint}");
+            //Console.WriteLine("Итог:");
+            //Console.WriteLine($"Оставшеeся здоровье {footMan.Name}: {footMan.RealHealthPoint}");
+            //Console.WriteLine($"Оставшеeся здоровье {targetFootMan.Name}: {targetFootMan.RealHealthPoint}");
             //Console.WriteLine($"Оставшеeся количество маны: {mage.Mane}");
         }
         static void Fight(Unit attackUnit, Unit targetUnit)
         {
             while (attackUnit.RealHealthPoint > 0 && targetUnit.RealHealthPoint > 0)
             {
+                Thread.Sleep(100);
                 attackUnit.Attack(targetUnit);
                 Console.WriteLine($"Здоровье {targetUnit.Name} после атаки: {targetUnit.RealHealthPoint}");
             }
+            Task.WhenAll();
         }
     }
 }
