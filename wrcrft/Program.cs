ae427a34b6ab53a2480b5461c12e8140240dfa80
zubr radel <gggg26705@gmail.com>
2023-01-13 18:55:40 +0300

    Енд потоков

--- wrcrft/Program.cs
+++ wrcrft/Program.cs
@@ -1,64 +1,75 @@
 ﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+using System.IO;
 
 namespace wrcrft
 {
     class Program
     {
+        
         static void Main()
         {
+           //Первый Бой
             UnitMaker unitMaker = new UnitMaker();
-            Unit footMan = unitMaker.Make("footMan");
-            footMan.Name = "Юра";
-            Unit targetFootMan = unitMaker.Make("footMan");
-            targetFootMan.Name = "Влад";
-            //Unit Archer = unitMaker.Make("archer");
-            //Mage mage = unitMaker.MagicMake();
-            /*BlackSmith smith = new BlackSmith(unitMaker);*/
-            
-            Console.WriteLine($"Урон {footMan.Name}: {footMan.Damage}");
-            Console.WriteLine($"Броня {footMan.Name}: {footMan.Armor}");
-            Console.WriteLine($"Урон {targetFootMan.Name}: {targetFootMan.Damage}");
-            Console.WriteLine($"Броня {targetFootMan.Name}: {targetFootMan.Armor}");
-            Task.Run(() => Fight(targetFootMan, footMan));
-            Task.Run(() => Fight(footMan, targetFootMan)).Wait();
+            Unit firstFootMan = unitMaker.Make("footMan");
+            firstFootMan.Name = "ПервыйПерсонажПервогоБоя";
+            Unit firstTargetFootMan = unitMaker.Make("footMan");
+            firstTargetFootMan.Name = "ВторойПерсонажПервогоБоя";
+            Task[] tasks = new Task[4];
+            Console.WriteLine($"Урон {firstFootMan.Name}: {firstFootMan.Damage}");
+            Console.WriteLine($"Здоровье {firstFootMan.Name}: {firstFootMan.Health}");
+            Console.WriteLine($"Урон {firstTargetFootMan.Name}: {firstTargetFootMan.Damage}");
+            Console.WriteLine($"Здоровье {firstTargetFootMan.Name}: {firstTargetFootMan.Health}");
+            Console.WriteLine();
 
-            //BlackSmith smith = new BlackSmith(unitMaker);
-            //smith.UpgradeDamage();
-            //smith.UpgradeArmor();
+            //Второй бой
+            Unit secondFootMan = unitMaker.Make("footMan");
+            secondFootMan.Name = "ПервыйПерсонажВторогоБоя";
+            Unit secondTargetFootMan = unitMaker.Make("footMan");
+            secondTargetFootMan.Name = "ВторойПерсонажВторогоБоя";
+            Console.WriteLine($"Урон {secondFootMan.Name}: {secondFootMan.Damage}");
+            Console.WriteLine($"Здоровье {secondFootMan.Name}: {secondFootMan.Health}");
+            Console.WriteLine($"Урон {secondTargetFootMan.Name}: {secondTargetFootMan.Damage}");
+            Console.WriteLine($"Здоровье {secondTargetFootMan.Name}: {secondTargetFootMan.Health}");
+            Console.WriteLine();
+            tasks[0] = Task.Run(() => Fight(firstTargetFootMan, firstFootMan, 1));
+            Thread.Sleep(200);
+            tasks[1] = Task.Run(() => Fight(firstFootMan, firstTargetFootMan, 1));
+            Thread.Sleep(200);
+            tasks[2] = Task.Run(() => Fight(secondTargetFootMan, secondFootMan, 2));
+            Thread.Sleep(200);
+            tasks[3] = Task.Run(() => Fight(secondFootMan, secondTargetFootMan, 2));
+            Thread.Sleep(200);
+            Task.WaitAll(tasks);
 
-            //Console.WriteLine($"Урон footMan после улучшения: {footMan.Damage}");
-            //Console.WriteLine($"Броня footMan полсе улучшения: {footMan.Armor} \n");
 
 
-            //Console.WriteLine($"Полное здоровье footMan: {footMan.FullHealthPoint}");
-            //Console.WriteLine($"Полное здоровье targetFootMan: {targetFootMan.FullHealthPoint}");
-            //Console.WriteLine($"Количество маны у mage: {mage.Mane} \n");
-            //while (targetFootMan.RealHealthPoint > 0 && footMan.RealHealthPoint > 0)
-            //{
-            //    targetFootMan.Attack(footMan);
-            //    Console.WriteLine($"Здоровье footMan после атаки: {footMan.RealHealthPoint}");
 
-            //    footMan.Attack(targetFootMan);
-            //    Console.WriteLine($"Здоровье targetFootMan после атаки: {targetFootMan.RealHealthPoint}");
-
-            //    mage.targetHeal(footMan);
-            //    Console.WriteLine($"Здоровье footMan после лечения: {footMan.RealHealthPoint}");
-            //    Console.WriteLine($"Оставшееся количество маны: {mage.Mane} \n");
-            //}
-            //Console.WriteLine("Итог:");
-            //Console.WriteLine($"Оставшеeся здоровье {footMan.Name}: {footMan.RealHealthPoint}");
-            //Console.WriteLine($"Оставшеeся здоровье {targetFootMan.Name}: {targetFootMan.RealHealthPoint}");
-            //Console.WriteLine($"Оставшеeся количество маны: {mage.Mane}");
         }
-        static void Fight(Unit attackUnit, Unit targetUnit)
+        static void Fight(Unit attackUnit, Unit targetUnit, int l)
         {
+            
             while (attackUnit.RealHealthPoint > 0 && targetUnit.RealHealthPoint > 0)
             {
-                Thread.Sleep(100);
-                attackUnit.Attack(targetUnit);
-                Console.WriteLine($"Здоровье {targetUnit.Name} после атаки: {targetUnit.RealHealthPoint}");
+                if(l == 1)
+                {
+                    Console.WriteLine("--------Первый--Бой-----------");
+                    attackUnit.Attack(targetUnit);
+                    Console.WriteLine($"Здоровье {targetUnit.Name} после атаки: {targetUnit.RealHealthPoint}");
+                    Console.WriteLine("------------------------------");
+                    
+                }
+                else if(l == 2)
+                {
+                    Console.WriteLine("--------Второй--Бой-----------");
+                    attackUnit.Attack(targetUnit);
+                    Console.WriteLine($"Здоровье {targetUnit.Name} после атаки: {targetUnit.RealHealthPoint}");
+                    Console.WriteLine("------------------------------");
+                    
+                }
+                Thread.Sleep(500);
             }
-            Task.WhenAll();
+            
         }
     }
 }
